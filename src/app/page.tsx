576691c0ffde102cfcfea75cee26310707cf0461
Mahedihasanimam <mdmehedihasen678@gmail.com>
2024-09-17 12:14:52 +0600

    shadcn calender added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,29 @@
+"use client";
 import Accordion from "@/components/Accordion";
 import Hero from "@/components/home/Hero";
 import { Button } from "@/components/ui/button";
+import { Calendar } from "@/components/ui/calendar";
+
+import React, { useState } from "react";
 
 export default function Home() {
+  const [date, setDate] = React.useState<Date | undefined>(new Date());
 
   return (
     <main>
       <div className="container mx-auto mb-12">
-        <Hero/>
-        <Accordion/>
-       
-        
+        <Hero />
+
+        <div className="flex items-center justify-between border-2 border-gray-200 rounded-md p-4 my-4 mb-12">
+          <Calendar
+
+            mode="single"
+            selected={date}
+            onSelect={setDate}
+            className="rounded-md border shadow-lg shadow-blue-500 transition-shadow duration-300"
+          />
+          <Accordion />
+        </div>
       </div>
     </main>
   );
--- src/components/Accordion.tsx
+++ src/components/Accordion.tsx
@@ -1,37 +1,36 @@
 import {
-    AlertDialog,
-    AlertDialogAction,
-    AlertDialogCancel,
-    AlertDialogContent,
-    AlertDialogDescription,
-    AlertDialogFooter,
-    AlertDialogHeader,
-    AlertDialogTitle,
-    AlertDialogTrigger,
-  } from "@/components/ui/alert-dialog"
-  
-const Accordion = () => {
-    return (
-        <div >
-            <AlertDialog>
-  <AlertDialogTrigger>Click Me</AlertDialogTrigger>
-  <AlertDialogContent className="bg-white text-black dark:bg-gradient-to-l from-blue-500  to-blue-200 dark:text-white">
-    <AlertDialogHeader>
-      <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
-      <AlertDialogDescription>
-        This action cannot be undone. This will permanently delete your account
-        and remove your data from our servers.
-      </AlertDialogDescription>
-    </AlertDialogHeader>
-    <AlertDialogFooter>
-      <AlertDialogCancel>Cancel</AlertDialogCancel>
-      <AlertDialogAction>Continue</AlertDialogAction>
-    </AlertDialogFooter>
-  </AlertDialogContent>
-</AlertDialog>
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "@/components/ui/alert-dialog";
 
-        </div>
-    );
+const Accordion = () => {
+  return (
+    <div>
+      <AlertDialog>
+        <AlertDialogTrigger className="bg-slate-400 text-white font-bold shadow-blue-400  shadow-lg px-6 py-2 rounded-md ">Click Me</AlertDialogTrigger>
+        <AlertDialogContent className="bg-white text-black dark:bg-gradient-to-l from-blue-500  to-blue-200 dark:text-white">
+          <AlertDialogHeader>
+            <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
+            <AlertDialogDescription>
+              This action cannot be undone. This will permanently delete your
+              account and remove your data from our servers.
+            </AlertDialogDescription>
+          </AlertDialogHeader>
+          <AlertDialogFooter>
+            <AlertDialogCancel>Cancel</AlertDialogCancel>
+            <AlertDialogAction>Continue</AlertDialogAction>
+          </AlertDialogFooter>
+        </AlertDialogContent>
+      </AlertDialog>
+    </div>
+  );
 };
 
 export default Accordion;
